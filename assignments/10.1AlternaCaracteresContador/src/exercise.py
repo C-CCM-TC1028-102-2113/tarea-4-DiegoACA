e8726a05405dd203744777d57f06dd38c8826a12
DiegoACA <a01656065@itesm.mx>
2021-09-04 22:34:30 -0500

    update

--- assignments/10.1AlternaCaracteresContador/src/exercise.py
+++ assignments/10.1AlternaCaracteresContador/src/exercise.py
@@ -1,6 +1,15 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    n = int(input("#"))
+
+    if n >= 0:
+        for x in range(1, n+1):
+            if x % 2 == 1:
+                print(str(x) + "-#")
+            elif x % 2 == 0:
+                print(str(x) + "-%")
+    else:
+        print("datos invalidos")
 
 if __name__=='__main__':   
     main()
